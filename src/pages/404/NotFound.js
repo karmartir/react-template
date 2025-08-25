798d66f18d09819caa96389ddd7fec0d421b653c
karmartir <karmartir@gmail.com>
2025-08-25 19:28:09 -0400

    404 Not found page

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="--center-all" style={{ minHeight: "80vh" }}>
+      <h2>Page Not Found</h2>
+      <p>The page you were looking for could not be found.</p>
+      <br />
+      <Link to="/">&#8592; Back to Home</Link>
+    </div>
+  );
 };
 
 export default NotFound;
