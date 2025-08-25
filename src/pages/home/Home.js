705ec0a3959fcf704f1ecdbe14d5ac89ab1101ab
karmartir <karmartir@gmail.com>
2025-08-25 19:20:29 -0400

    Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import "./Home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  const name = "User";
+  //  const { name } = useSelector((state) => state.data);
+
+  return (
+    <div className="home">
+      <div className="container">
+        <h2>Home Page</h2>
+        <p>
+          Welcome to the <b>Home</b> page
+        </p>
+        <p>
+          This page has <b>Redux Toolkit</b> set up.
+        </p>
+        <p>
+          The name: <b>{name}</b> is stored in the dataSlice
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
