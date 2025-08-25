78495b074ead1cb98aea9c08f06f178883d47f00
karmartir <karmartir@gmail.com>
2025-08-25 17:45:20 -0400

    starting project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/404/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
