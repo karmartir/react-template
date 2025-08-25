8dc407d84673d412799747bb24c8c9672ac9d9a6
karmartir <karmartir@gmail.com>
2025-08-25 19:10:36 -0400

    Footer with dynamic date

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
+import styles from "./Footer.module.scss";
+
+const date = new Date();
+export const year = date.getFullYear();
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className={styles.footer}>&copy; {year} All Rights Reserved</div>;
 };
 
 export default Footer;
