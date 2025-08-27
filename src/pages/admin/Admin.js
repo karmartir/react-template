68454f94b7320cfaed21b7ef2dcc510b26402870
karmartir <karmartir@gmail.com>
2025-08-27 17:27:58 -0400

    Setup Admin Page

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./Admin.module.scss";
+import Sidebar from "../../components/admin/sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../components/admin/home/Home";
+import Profile from "../../components/admin/profile/Profile";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className={styles.admin}>
+      <div className={styles.navbar}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
