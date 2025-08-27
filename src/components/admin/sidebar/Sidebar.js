ebfd461868e73ccdc23d157170ff7a17b16ebfa9
karmartir <karmartir@gmail.com>
2025-08-27 17:58:19 -0400

    Sidebar Component

--- src/components/admin/sidebar/Sidebar.js
+++ src/components/admin/sidebar/Sidebar.js
@@ -1,7 +1,57 @@
+import "./Sidebar.scss";
 import React from "react";
+import { useState } from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { FaLink } from "react-icons/fa";
+import menu from "../../../data/sidebar";
+import SidebarItem from "./SidebarItem";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
+  const closeSidebar = () => setIsOpen(false);
+  const openSidebar = () => setIsOpen(true);
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/admin/home");
+    // closeSidebar();
+  };
+
+  return (
+    <div className="layout">
+      <div
+        className={isOpen ? "sidebar-overlay show-overlay" : "sidebar-overlay"}
+        onClick={closeSidebar}
+      ></div>
+      <div
+        className="sidebar"
+        style={{ width: isOpen ? "230px" : "60px" }}
+        onMouseEnter={openSidebar}
+        onMouseLeave={closeSidebar}
+      >
+        <div className="top_section">
+          <div className="logo" style={{ display: isOpen ? "block" : "none" }}>
+            <FaLink size={30} onClick={goHome} style={{ cursor: "pointer" }} />
+          </div>
+          <div
+            className="bars"
+            style={{ marginLeft: isOpen ? "135px" : "0px" }}
+          >
+            <HiMenuAlt3 onClick={toggle} />
+          </div>
+        </div>
+        {menu.map((item, index) => (
+          <SidebarItem
+            key={index}
+            item={item}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
