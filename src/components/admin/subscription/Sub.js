481f585f8ba8d73d157be96f13d74381d9276507
karmartir <karmartir@gmail.com>
2025-08-27 19:56:21 -0400

    Subscription Component

---
+++ src/components/admin/subscription/Sub.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Sub.scss";
+import { plans } from "../../../data/plans";
+
+const Sub = () => {
+  return <div>Sub</div>;
+};
+
+export default Sub;
--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -3,6 +3,7 @@ import Sidebar from "../../components/admin/sidebar/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Home from "../../components/admin/home/Home";
 import Profile from "../../components/admin/profile/Profile";
+import Sub from "../../components/admin/subscription/Sub";
 
 const Admin = () => {
   return (
@@ -14,6 +15,7 @@ const Admin = () => {
         <Routes>
           <Route path="home" element={<Home />} />
           <Route path="profile" element={<Profile />} />
+          <Route path="account" element={<Sub />} />
         </Routes>
       </div>
     </div>
