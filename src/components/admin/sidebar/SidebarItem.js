aa4e3dfbebdcc74c61e3b5d1b14c5e1b609a7a11
karmartir <karmartir@gmail.com>
2025-08-27 18:14:22 -0400

    Sidebar item component part1

--- src/components/admin/sidebar/SidebarItem.js
+++ src/components/admin/sidebar/SidebarItem.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { DiVim } from "react-icons/di";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
-const SidebarItem = () => {
-  return <div>SidebarItem</div>;
+const activeLink = ({ isActive }) => (isActive ? "active" : "link");
+const activeSublink = ({ isActive }) => (isActive ? "active" : "link");
+
+const SidebarItem = ({ item, isOpen, setIsOpen }) => {
+  const [expandMenu, setExpandMenu] = useState(false);
+
+  useEffect(() => {
+    if (!isOpen) {
+      setExpandMenu(false);
+    }
+  }, [isOpen, setExpandMenu]);
+
+  const hideSidebar = () => {
+    setExpandMenu(false);
+    setIsOpen(false);
+  };
+
+  if (item.children) {
+    return <div></div>;
+  } else {
+    return (
+      <NavLink to={item.path} className={activeLink}>
+        <div className="sidebar-item s-parent" onClick={hideSidebar}>
+          <div className="sidebar-title">
+            <span>
+              {item.icon && <div className="icon">{item.icon}</div>}
+              {isOpen && <div>{item.title}</div>}
+            </span>
+          </div>
+        </div>
+      </NavLink>
+    );
+  }
 };
 
 export default SidebarItem;
