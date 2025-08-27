0361a17a96a0dd887c754320f4f1c0e0d8dde277
karmartir <karmartir@gmail.com>
2025-08-27 18:51:18 -0400

    Dashboard Profile Page

--- src/components/admin/home/Home.js
+++ src/components/admin/home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h3>--- Dashboard ---</h3>
+      <p>
+        {" "}
+        This is the user's <b>Dashboard Page</b>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/admin/profile/Profile.js
+++ src/components/admin/profile/Profile.js
@@ -1,7 +1,27 @@
-import React from "react";
+import "./Profile.scss";
+import Card from "./../../card/Card";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="profile">
+      <h3>--- Profile ---</h3>
+      <Card cardClass={"card"}>
+        <form>
+          <p>
+            <label>Name:</label>
+            <input type="text" placeholder="Name" name="name" />
+          </p>
+          <p>
+            <label>Email:</label>
+            <input type="email" placeholder="Email" name="email" />
+          </p>
+          <button className="--btn --btn-primary --btn-block">
+            Update Profile
+          </button>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default Profile;
